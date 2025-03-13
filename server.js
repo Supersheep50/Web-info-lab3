3ebd484ea28a3f2a301953b85a6279b64b7ab55b
jonsmacbookair <jonathan.morrisseyto@gmail.com>
2025-03-13 11:03:23 +0000

    Setting up db

--- server.js
+++ server.js
@@ -1,24 +1,59 @@
 const mysql = require('mysql');
+const express = require('express');
+const cors = require('cors');
 
+const app = express();
+app.use(cors());
+app.use(express.json()); // handling json data from react
+
+// connect to phpadmin db
 
 const connection = mysql.createConnection({
 
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'CS230_2025'
+    database: 'cs230_p250126'
 });
 
-connection.connect();
+connection.connect(err => {
 
-connection.query("INSERT INTO students (name, email, grade) VALUES ('John', 'jonathan@gmail.com', 9)",
-function (error, results, fields) {
-    if (error) {
-    console.log(error);
+if(error){
+    console.log("Connection failed", error);
 } else {
-    console.log(results);
-}}); 
+    console.log("Connected to database");
+}
+});
 
-// node.server.js in terminal 
+// insert an artist 
+
+app.post('/artists', (req, res) => {
+    const {name, monthly_listeners, genre} = req.body;
+    const sql = 'INSERT INTO artists (name, monthly_listeners, genre) VALUES (?, ?, ?)';
+    connection.query[sql, [name, monthly_listeners, genre], (error, results) => {
+        if(error){
+            res.status(500).json({message: 'Error inserting artist'});
+        } else {
+            res.status(200).json({message: 'Artist inserted'});
+        }}
+    ];
+});
+
+// get all artists
+
+app.get('/artists', (req, res) => {
+    connection.query('SELECT * FROM artists', (error, results) => {
+        if(error){
+            res.status(500).json({message: 'Error getting artists'});
+        } else {
+            res.status(200).json(results);
+        }
+    });
+});
+
+// update an artist
+
+
+
 
 
